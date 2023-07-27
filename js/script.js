6392cad980b5c3c4e9513b9ed71102aa0fdf1452
Fatama Alam Tonny <84072274+tonnycse@users.noreply.github.com>
2023-07-27 09:49:56 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1";
+const APILINK = "https://api.t hemoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0bf325694337f49b3dbcb140311cb2e9&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=0bf325694337f49b3dbcb140311cb2e9&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
