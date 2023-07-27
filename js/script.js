e60a2777ff599ecacd8bbf8827137d758acbf51f
Fatama Alam Tonny <84072274+tonnycse@users.noreply.github.com>
2023-07-27 10:07:02 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,23 +12,23 @@ function returnMovies(url){
     .then(function(data){
     console.log(data.results);
     data.results.forEach(element => {
-        const div_card = document.createElement("div");
-        div_card.setAttribute("class", "card");
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
 
-        const div_row = document.createElement("div");
-        div_row.setAttribute("class", "row");
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class', 'row');
 
-        const div_column = document.createElement("div");
-        div_column.setAttribute("class", "column");
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class', 'column');
 
-        const image = document.createElement("img");
-        image.setAttribute("class", "thumbnail");
-        image.setAttribute("id", "image");
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
 
-        const title = document.createElement("h3");
-        title.setAttribute("id", "title");
+        const title = document.createElement('h3');
+        title.setAttribute('id', 'title');
 
-        const center = document.createElement("center");
+        const center = document.createElement('center');
 
         title.innerHTML = '${element.title}';
         image.src = IMG_PATH + element.poster_path;
@@ -46,7 +46,7 @@ function returnMovies(url){
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    main.innerHTML = "";
+    main.innerHTML = '' ;
     
     const searchTerm = search.value;
     
@@ -55,4 +55,4 @@ form.addEventListener("submit", (e) => {
         search.value = "";
     } 
     
-    }
+    });
