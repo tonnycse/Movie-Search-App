84e5d48ea1ef1028639bbdf34ac3473f7826a0d7
Fatama Alam Tonny <84072274+tonnycse@users.noreply.github.com>
2023-07-28 15:45:44 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function returnMovies(url){
 
         center.appendChild(image);
         div_card.appendChild(center);
-        title.appendChild(title);
+        div_card.appendChild(title);
         div_column.appendChild(div_card);
         div_row.appendChild(div_column);
 
